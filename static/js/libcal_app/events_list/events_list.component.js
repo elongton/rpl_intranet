f3ad87e2346db496d006f5daa27c371f0fd8b7fb
Max Longton <elongton@vt.edu>
2017-10-14 20:59:25 -0400

    added http method to events_list.component.js

--- static/js/libcal_app/events_list/events_list.component.js
+++ static/js/libcal_app/events_list/events_list.component.js
@@ -7,6 +7,27 @@ angular.module('events').
           $scope.branch = $cookies.get("branch")
           $scope.username = $cookies.get("username")
           $scope.staticfiles = staticfiles;
+
+          // HTTP WAY OF DOING THINGS
+          $scope.getcreds = function(){
+            var createUrl = 'https://api2.libcal.com/1.1/oauth/token'
+            var req = {
+                          method: "POST",
+                          url: createUrl,
+                          data: {
+                            client_id: '135',
+                            client_secret: 'blah',
+                            grant_type: 'client_credentials',
+                          },
+                          headers: {
+                            // authorization: "JWT " + token
+                          },
+                      }//req
+
+            var requestAction = $http(req).then(successCallback, errorCallback)
+
+          }
+
         }//controller
 
         });
