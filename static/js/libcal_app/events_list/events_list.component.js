ef3da286060984a0fe849045e06584ad45a07229
Max Longton <elongton@vt.edu>
2017-10-15 00:49:11 -0400

    added correct authentication algorithms to events_list.component

--- static/js/libcal_app/events_list/events_list.component.js
+++ static/js/libcal_app/events_list/events_list.component.js
@@ -3,31 +3,66 @@
 angular.module('events').
       component('eventsList', {
         templateUrl: '/api/templates/libcal_app/events_list.html',
-        controller: function($scope, $cookies, $location){
+        controller: function($scope, $cookies, $location,$http){
           $scope.branch = $cookies.get("branch")
           $scope.username = $cookies.get("username")
           $scope.staticfiles = staticfiles;
 
-          // HTTP WAY OF DOING THINGS
+
+          // PULL DATA
+          $scope.pullspaces = function(){
+            function successCallback(response) {
+                console.log('success!')
+                console.log(response)
+              }
+            function errorCallback(response) {
+                console.log(response)
+              }
+            var endpoint = 'https://api2.libcal.com/1.1/space/bookings?lid=1598&limit=20&date=2017-10-18&formAnswers=1'
+            var req = {
+                          method: "GET",
+                          url: endpoint,
+                          headers: {
+                            authorization: "Bearer " + $scope.libcaltoken
+                          },
+                      }//req
+            var requestAction = $http(req).then(successCallback, errorCallback)
+          }
+
+
+
+
+
+          // OBTAIN THE ACCESS TOKEN!
           $scope.getcreds = function(){
-            var createUrl = 'https://api2.libcal.com/1.1/oauth/token'
+            function successCallback(response) {
+                console.log('success!')
+                console.log(response)
+                $scope.libcaltoken = response.data.access_token
+              }
+            function errorCallback(response) {
+                console.log(response)
+              }
+            var token_url = 'https://api2.libcal.com/1.1/oauth/token'
             var req = {
                           method: "POST",
-                          url: createUrl,
+                          url: token_url,
                           data: {
                             client_id: '135',
-                            client_secret: 'blah',
+                            client_secret: '906a3eab0c7f08ebad67e5160f0ae951',
                             grant_type: 'client_credentials',
                           },
                           headers: {
                             // authorization: "JWT " + token
                           },
                       }//req
-
             var requestAction = $http(req).then(successCallback, errorCallback)
-
           }
 
         }//controller
 
         });
+
+
+
+        // https://api2.libcal.com/1.1/space/locations
