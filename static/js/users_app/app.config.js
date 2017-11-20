26d1f8e021bcfb2eea4387984d387ad1b6ae514a
Max Longton <elongton@Maxs-MacBook-Pro.local>
2017-11-19 22:22:42 -0500

    ready to add events to libcal lister

--- static/js/users_app/app.config.js
+++ static/js/users_app/app.config.js
@@ -8,13 +8,27 @@ angular.module('usersApp').
                 $routeProvider,
               ){
 
-                var onlyLoggedIn = function ($location,$q, $cookies) {
+                var onlyLoggedIn = function (User, $location,$q, $cookies,) {
                     var deferred = $q.defer();
-                    if ($cookies.get("token")) {
-                        deferred.resolve();
-                    } else {
-                        deferred.reject();
-                        $location.url('/users/login');
+                    if ($cookies.get("token")){
+                      User.tokenVerify({token: $cookies.get("token")}).$promise.then(
+                        function(success){
+                          deferred.resolve();
+                        },//success,
+                        function(error){
+                          rejection(deferred);
+                          console.log(error)
+                        }//error
+                      )//user.tokenVerify
+                    }else{
+                      rejection(deferred);
+                    }//else
+
+                    function rejection(deferral){
+                      deferral.reject();
+                      var url = "/users/login";
+                      window.location = url;
+                      window.location.replace(url);
                     }
                     return deferred.promise;
                 };
