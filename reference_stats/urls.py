d69a846994570c6d91773e6dd4b09cc785fa9fef
Max Longton <elongton@vt.edu>
2017-12-18 00:45:30 -0500

    removed normal csv view froom urls.ppy

--- reference_stats/urls.py
+++ reference_stats/urls.py
@@ -6,12 +6,11 @@ from .views import (
     BigData,
     BigDataChart,
     some_streaming_csv_view,
-    normal_csv,
+    # normal_csv,
 )
 
 urlpatterns = [
     url(r'^streamingcsvtest/$',some_streaming_csv_view, name="stream"),
-    url(r'^csvtest/$',normal_csv, name="csv"),
     url(r'^bigdata/(?P<date1>\d{4}-\d{2}-\d{2})/(?P<date2>\d{4}-\d{2}-\d{2})/(?P<branch>[A-Za-z\+\/]+)/$', BigData, name="bigdata"),
     url(r'^bigdatachart/(?P<date1>\d{4}-\d{2}-\d{2})/(?P<date2>\d{4}-\d{2}-\d{2})/(?P<branch>[A-Za-z\+\/]+)/$', BigDataChart, name="bigdatachart"),
 ]
