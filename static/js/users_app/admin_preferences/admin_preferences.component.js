4f32523936d35b780499a75304ba56192b46940b
Max Longton <elongton@vt.edu>
2018-01-04 20:19:30 -0500

    still working on edit user: need to add the put async call, do some error handling, disallow bootstrap modal from going away on click

--- static/js/users_app/admin_preferences/admin_preferences.component.js
+++ static/js/users_app/admin_preferences/admin_preferences.component.js
@@ -5,47 +5,42 @@ angular.module('admin_preferences').
     templateUrl: '/api/templates/users_app/admin_preferences.html',
     controller: function(Data, User, $filter, $cookies,$http,$location,$routeParams,$rootScope,$scope,$route,){
       //startup
+      $scope.deletecrucible = false
 
-      Data.getBranches().$promise.then(
-                function(result){
-                  $scope.branches = result
-                  $scope.usercreate = {
-                    selected_branch: $scope.branches[0],
-                    admin: false,}
-
-                    //sequence...
-                    Data.getUsers().$promise.then(
-                              function(result){
-                                $scope.users = {
-                                  availableOptions: result,
-                                  selectedOption: result[0]
-                                }
-                                $scope.useredit = {
-                                  branch: $filter('filter')($scope.branches, $scope.users.selectedOption.branch)[0],
-                                }
-                              },
-                              function(error){console.log(error)});
-                },
-                function(error){console.log(error)});
-                //watch
-                //watch to see if different user is selected.
-                $scope.$watch(function(){
-                  if ($scope.users){
+      var getUsers = function(){
+        Data.getUsers().$promise.then(
+                  function(result){
+                    $scope.users = {
+                      availableOptions: result,
+                      selectedOption: result[0]
+                    }
                     $scope.useredit = {
                       branch: $filter('filter')($scope.branches, $scope.users.selectedOption.branch)[0],
-                    }//$scope.useredit
-                  }//if
-                })//$scope.watch
+                    }
+                  },
+                  function(error){console.log(error)});
+      }
+      //get some data
+      Data.getBranches().$promise.then(
+        function(result){
+          $scope.branches = result
+          $scope.usercreate = {
+            selected_branch: $scope.branches[0],
+            admin: false,}
 
+            //sequence...
+            getUsers();
 
-
-
-
-                // $scope.useredit = {
-                //   branch: $filter('filter')($scope.branches, $scope.users.selectedOption.branch)[0],
-                // }
-
-
+        },
+        function(error){console.log(error)});
+      //watch to see if different user is selected.
+      $scope.$watch(function(){
+        if ($scope.users){
+          $scope.useredit = {
+            branch: $filter('filter')($scope.branches, $scope.users.selectedOption.branch)[0],
+          }//$scope.useredit
+        }//if
+      })//$scope.watch``
 
       //edit user code
 
@@ -70,10 +65,22 @@ angular.module('admin_preferences').
       }//$scope.createUser
 
       //delete user code
-      // $scope.deleteUser = function(user_id){
-      //   User.userDelete({id: user_id}).$promise.then(function)
-      // }
 
+      $scope.deletetoggle = function(){
+        $scope.deletecrucible = !$scope.deletecrucible;
+      }
+
+      $scope.deleteUser = function(user_id){
+        User.userDelete({id: user_id}).$promise.then(
+          function(success){
+            console.log(success)
+            getUsers();
+        },
+        function(error){console.log(error)}
+      )};
+
+
+//deleteUser(users.selectedOption.id)
 
 
 }});
